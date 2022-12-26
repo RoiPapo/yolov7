0567905d515b7d4d4eb4ecca9c3bf58050645eeb
pereztomer <pereztomer3@gmail.com>
2022-12-26 11:46:34 +0200

    class imbalance analysis

--- raw_data_analysis/calc_class_balance.py
+++ raw_data_analysis/calc_class_balance.py
@@ -1,5 +1,7 @@
 from glob import glob
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.ticker import PercentFormatter
 
 
 def extract_files_labels(file_path):
@@ -13,14 +15,14 @@ def extract_files_labels(file_path):
     return labels
 
 
-def calc_class_balance():
-    ds_labels_path = '/home/tomer/PycharmProjects/yolov7/raw_data_ex1/HW1_dataset/bboxes_labels'
+def calc_class_balance(ds_labels_path, graph_title):
     ds_labels_files = glob(f'{ds_labels_path}/**')
     ds_labels_per_file = [extract_files_labels(file) for file in ds_labels_files]
     complete_labels = []
     for label_pair in ds_labels_per_file:
         complete_labels += label_pair
 
+    # complete_labels = [int(x) for x in complete_labels]
     class_names = ['Right\nScissors',
                    'Left\nScissors',
                    'Right\nNeedle\ndriver',
@@ -35,12 +37,28 @@ def calc_class_balance():
     # Create the histogram
     plt.hist(complete_labels)
 
+    # add title
+    plt.title(graph_title)
+
     # Set the labels for the x-axis tick marks
     plt.xticks([0, 1, 2, 3, 4, 5, 6, 7], class_names)
 
     # Show the plot
     plt.show()
 
+    plt.hist(complete_labels, weights=np.ones(len(complete_labels)) / len(complete_labels))
+
+    plt.title(f'percentages {graph_title}')
+
+    plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
+    plt.xticks([0, 1, 2, 3, 4, 5, 6, 7], class_names)
+    plt.show()
+
+
 
 if __name__ == '__main__':
-    calc_class_balance()
+    general_ds_path = '/home/tomer/PycharmProjects/yolov7/raw_data_ex1'
+    for set_type in ('train', 'validation', 'test'):
+        set_ds_path = f'{general_ds_path}/{set_type}/bboxes_labels'
+        calc_class_balance(set_ds_path, f'{set_type} class count')
+        exit()
